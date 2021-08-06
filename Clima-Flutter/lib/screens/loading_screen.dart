b1fffa7d1e29197d5de6e4c96962a8672b397d02
Palash Agarwal <palashagarwal@Palashs-MacBook-Air.local>
2021-08-06 22:35:53 +0530

    second commit

--- Clima-Flutter/lib/screens/loading_screen.dart
+++ Clima-Flutter/lib/screens/loading_screen.dart
@@ -15,13 +15,29 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getLocation() async {
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.low);
-    print(position);
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.low);
+      print(position);
+    } catch (e) {
+      print(e);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    String myMargin = '15';
+    double myMarginAsADouble;
+    try {
+      myMarginAsADouble = double.parse(myMargin);
+    } catch (e) {
+      print(e);
+    }
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.all(myMarginAsADouble ?? 30.0),
+        color: Colors.red,
+      ),
+    );
   }
 }
